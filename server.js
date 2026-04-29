d377746ad110e6cb8f3047ca9ae12052351623d2
gpizarrom3 <gpizarrom.3@gmail.com>
2026-04-29 08:46:27 -0400

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.post('/api/diagnostico', async (req, res) => {
         // USAMOS EL MODELO QUE APARECE PRIMERO EN TU LISTA
         // Cambiamos v1 por v1beta porque es donde aparece listado tu modelo
         const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${API_KEY}`;
+
         const response = await fetch(url, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
